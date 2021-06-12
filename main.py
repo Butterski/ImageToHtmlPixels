56412c4c577b8b2feec3e1c103ba27e612340564
butterski <miloszkucharski@gmail.com>
2021-06-13 00:30:57 +0200

    main commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from PIL import Image
+import json
+import string
+
+im = Image.open('image.png').convert('RGB')
+
+with open("template.html") as t:
+    template = string.Template(t.read())
+
+width, height = im.size
+
+
+def rgb2hex(r, g, b):
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
+
+
+def rgb_of_pixel(x, y):
+    r, g, b = im.getpixel((x, y))
+    a = rgb2hex(r, g, b)
+    return a
+
+
+def generate_art(option, stretch):
+    content = ""
+    print("Generating...")
+    for y in range(0, height, int(option)):
+        content += f'<br>'
+        for x in range(0, width, int(option)):
+            content += f'<div class="pixel" style="background-color: {rgb_of_pixel(x, y)}"></div>'
+
+    #final_pixel_size = template.substitute(pixels=stretch)
+    final_output = template.substitute(divs=content, pixels=stretch)
+    with open("index.html", "w") as output:
+        output.write(final_output)
+
+
+print("Choose the quality of picture")
+print("1 - original quality")
+print("Bigger the number = worse quality")
+option_input = input()
+
+print("Choose the pixel size")
+stretch_input = input()
+
+generate_art(option_input, stretch_input)
+
+print("Done :)")
+print("If you inspect the page it may be a bit laggy too!")
